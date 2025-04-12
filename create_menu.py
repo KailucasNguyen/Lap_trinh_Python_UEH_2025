c1541691c73469df801a36a4d6f90fa31289451c
KailucasNguyen <152133939+kailucasnguyen@users.noreply.github.com>
2025-04-12 10:28:12 +0700

    Add files via upload

---
+++ create_menu.py
@@ -0,0 +1,59 @@
+import random
+
+
+
+def create_list(n):
+    list_random = []
+    for i in range(n):
+        list_random.append(random.randint(1, 100))
+    return list_random
+
+def menu(list_random):
+    print('''---------------------------Menu---------------------------
+1. In ra danh sách vừa tạo.
+2. In đảo ngược danh sách.
+3. Sắp xếp danh sách và in ra danh sách vừa sắp xếp (dùng sorted).
+4. Tìm phần tử lớn nhất của danh sách và vị trí phần tử lớn nhất cuối cùng.
+5. Đếm số lượng các phần tử bằng giá trị X nhập từ bàn phím. In ra các vị trí xuất hiện.
+6. In ra vị trí các phần tử là số nguyên tố.
+7. Tìm các số duy nhất (không trùng lặp) trong danh sách.
+8. Liệt kê các giá trị xuất hiện trong mảng kèm theo số lần xuất hiện của nó.
+9. In ra các đoạn con trong danh sách giảm liên tiếp.
+10. Thoát''')
+    choose = int(input("Chọn lựa của bạn <1..10>:"))
+    match choose:
+        case 1:
+            print(list_random)
+        case 2:
+            print(list_random[::-1])
+        case 3:
+            print(sorted(list_random))
+        case 4:
+            print(f'Phần tử lớn nhất trong danh sách: {max(list_random)}\nVị trí phần tử lớn nhất cuối cùng: {len(list_random) - list_random[::-1].index(max(list_random))}')
+        case 5:
+            num = int(input('Bạn muốn đếm số nào?:'))
+            dem = 0
+            index_hien_tai = 0
+            vi_tri = []
+            for i in list_random:
+                if i == num:
+                    dem += 1
+                    vi_tri.append(index_hien_tai)
+                index_hien_tai += 1
+            print(dem, vi_tri)
+        case 6:
+            pass
+        case 7:
+            pass
+        case 8:
+            pass
+        case 9:
+            pass
+
+
+
+if __name__ == '__main__':
+    n = int(input("Nhập số phần tử mảng n: "))
+    list_random = create_list(n)
+    while True:
+        menu(list_random)
